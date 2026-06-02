fe6642569536c62c521b35f1332cdaacaf28863f
Rikuto <mikadorikuto@gmail.com>
2026-06-02 22:26:53 +0900

    add some code

--- src/components/UpdatedLab.vue
+++ src/components/UpdatedLab.vue
@@ -1,3 +1,24 @@
+<script setup>
+import { ref, onUpdated } from 'vue';
+
+const messages = ref(['Initial message']);
+const chatBoxRef = ref(null);
+
+const addMessage = () => {
+    messages.value.push(`New message ${messages.value.length + 1}`)
+    console.log('Added a new data [UpdatedLab], which isnt updated in the DOM yet');
+}
+
+onUpdated(() => {
+    console.log('[UpdatedLab] onUpdated');
+
+    if (chatBoxRef.value) {
+        chatBoxRef.value.scrollTop = chatBoxRef.value.scrollHeight;
+        console.log('[UpdatedLab] chat box scrooled to the bottom')
+    }
+})
+</script>
+
 <template>
     
 </template>
